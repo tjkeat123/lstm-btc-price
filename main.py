4502418edfca14592ac09b54ec56b75489631305
TJ Keat <tjkeat123@gmail.com>
2025-07-05 02:11:39 +0800

    refactor: move plot_data() to src/utils/plotting.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from typing import Optional
 import random
 
+from src.utils.plotting import plot_data
+
 SEED = 42
 
 # Set seeds for reproducibility
@@ -19,23 +21,6 @@ random.seed(SEED)
 # Set TensorFlow session determinism for reproducibility
 tf.keras.utils.set_random_seed(SEED)
 tf.config.experimental.enable_op_determinism()
-
-def plot_data(df: pd.DataFrame):
-    os.makedirs("plots", exist_ok=True)
-    # plot the close price and volume over time
-    plt.figure(figsize=(10, 5))
-    plt.plot(df.index, df["open"], label="open", color="blue")
-    plt.plot(df.index, df["close"], label="close", color="red")
-    plt.legend()
-    plt.title("Open/Close Price over Time")
-    plt.savefig("plots/open_close_price.png")
-    plt.close()
-
-    plt.figure(figsize=(10, 5))
-    plt.plot(df.index, df["volume"], label="volume", color="orange")
-    plt.title("Volume over Time")
-    plt.savefig("plots/volume.png")
-    plt.close()
 
 def normalize(frame: pd.DataFrame):
     return frame.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
